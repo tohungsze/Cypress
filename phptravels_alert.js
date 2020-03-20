662068cd9ecc93cbae650af67451e759247bbab9
To Hung Sze <tohungsze.acton@gmail.com>
2020-03-19 21:52:25 -0400

    Added phptravels_alert.js

---
+++ phptravels_alert.js
@@ -0,0 +1,22 @@
+// techniques demonstrated:
+// handling drop down & popup
+//
+describe ('My test for phptravls', () => {
+
+  it ('launches', () => {
+    cy.visit ('/');
+  });
+
+  it ('Table', () => {
+    cy.visit ('/');  // visit '/' that is defined in cypress.json in project root
+    cy.contains('Alerts & Modals').click();  // find first element containing 'Alerts & Modals' and click
+    cy.contains('Bootstrap Modals').click();  // find first element containing 'Bootstrap Modals' and click
+    cy.get('[href="#myModal"]').click()     // cannot use contains here
+    // cy.get('nav').contains('Sign Out').should('have.attr', 'href', '/signout') - this is an example from doc
+    cy.get('[href="#myModal2"]').click();
+    //cy.wait(2000);
+    cy.contains('Save changes').click({ force: true });
+    //cy.wait(2000);
+  });
+
+});
--- phptravels_basic.js
+++ phptravels_basic.js
@@ -1,7 +1,3 @@
-// techniques demonstrated:
-// select element by .contains (by link text), .get (by id), css (type is button)
-
-
 describe ('My test for phptravls', () => {
 
   it ('launches', () => {
@@ -10,11 +6,11 @@ describe ('My test for phptravls', () => {
 
   it ('Input Forms', () => {
     cy.visit ('/');  // visit '/' that is defined in cypress.json in project root
-    cy.contains('Input Forms').click();  // find first element containing text 'Input Forms' and click
+    cy.contains('Input Forms').click();  // find first element containing 'Input Forms' and click
     cy.contains('Simple Form Demo').click();
     cy.get('#user-message')  // find the element with id user-message
-      .type('hello')
-    cy.get('button').contains('Show Message').click()  // find button containing text 'Show Message' by css type .button
+      .type('hello') 
+    cy.get('button').contains('Show Message').click()  // find button containing text 'Show Message'
 
   });
 
